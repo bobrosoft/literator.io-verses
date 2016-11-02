187c145c17e625e900cc23eb37c1943c0c8c6a43
bobrosoft <bobrosoft@yandex.ru>
2016-11-03 00:25:27 +0400

    CARD-92: Verse: add "isWellKnown" flag (update packager.js)

--- packager.js
+++ packager.js
@@ -80,7 +80,7 @@
       }
 
       // Check if all required attributes set
-      var missingAttributes = getMissingAttributes(verseMeta, ['name', 'title', 'year', 'mood']);
+      var missingAttributes = getMissingAttributes(verseMeta, ['name', 'title', 'year', 'mood', 'isWellKnown']);
       if (missingAttributes.length) {
         reportValidationError('verse\'s "meta.json" is not full, next attributes are missing: ' + missingAttributes.join(', '));
       }
